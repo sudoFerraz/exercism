ad3ffdd382b2914e2f4dd7f9b0634536e181ef79
root <ferraz@ufu.br>
2016-10-26 01:07:21 +0000

    All 50 tests passing

--- python/clock/clock.py
+++ python/clock/clock.py
@@ -1,16 +1,16 @@
-"""class Clock(object):
-    
-    def __init__(self, hour, minute):
-        self.hour = hour
-        self.minute = minute
-
-"""
 class Clock(object):
 
     def __str__(self):
         return self.relogio
 
     def __init__(self, hour, minute):
+        self.hour = hour
+        self.minute = minute
+        self.relogio = self.calcula(self.hour, self.minute)
+        self.calcula(hour, minute)
+        
+    def calcula(self, hour, minute):
+
         if hour >= 24:
             hour = hour % 24
         if hour < 0:
@@ -28,11 +28,14 @@ class Clock(object):
         if minute < 10 or minute > 59:
             minute = minute % 60
             minute = format(minute, "02")
-        
+        self.hour = hour
+        self.minute = minute
         self.relogio = str(hour) + ':' + str(minute)
+        return self.relogio
 
     def add(self, minute):
-        pass
+        self.relogio = self.calcula(int(self.hour), int(self.minute) + minute)
+        return self.relogio
 
     def __eq__(self, other):
         if self.relogio == other.relogio:
